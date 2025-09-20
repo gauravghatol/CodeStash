2bcb6bac3b12dcebce573926ce7db091aeacd183
gauravghatol <gauravghatol49@gmail.com>
2025-09-20 17:12:24 +0530

    Expand language list + unify form input styles for OTP pages

--- frontend/src/constants/languages.js
+++ frontend/src/constants/languages.js
@@ -2,9 +2,10 @@
 export const LANGUAGES = [
   'JavaScript','TypeScript','JSX','TSX',
   'Python','Java','C','C++','C#','Go','Rust','PHP','Ruby','Kotlin','Swift','Dart','R','Scala','Elixir','Haskell','Lua','Perl','Solidity','SQL','GraphQL',
-  'HTML','CSS','Sass','Less','Tailwind',
-  'Shell','Bash','PowerShell','Batchfile',
-  'YAML','JSON','Markdown','INI','TOML','XML','CSV','Makefile','Dockerfile','Nginx','Apache',
+  'Objective-C','Objective-C++','MATLAB','Julia','Fortran','COBOL','Erlang','OCaml','F#','Clojure','ReasonML','Scheme','Common Lisp','Prolog','Tcl','Assembly','Nim','Crystal','Zig','Ada','Pascal','Delphi','Smalltalk','ABAP','Apex','Groovy','Gradle','VB.NET','VBA','ASP.NET','PowerShell','Batchfile',
+  'HTML','XML','CSS','Sass','Less','Tailwind',
+  'Shell','Bash',
+  'YAML','JSON','Markdown','INI','TOML','CSV','Makefile','CMake','Dockerfile','Nginx','Apache','HCL','Terraform','Protobuf',
 ];
 
 export const DEFAULT_LANGUAGE = 'JavaScript';
